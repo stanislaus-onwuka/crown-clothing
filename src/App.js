0c993098e693653652f7f90fd5ad6b34d105aea3
Onwuka Stanley <kachi2505@yahoo.com>
2020-08-10 17:57:45 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop';
 import { Switch,Route } from 'react-router-dom';
 import Header from './components/header/header';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up"; 
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
